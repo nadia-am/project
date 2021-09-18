32a45e92a146abef7261243ecf71f68ae1dc4778
nadia <nadia.amoee@gmail.com>
2021-09-18 09:19:19 +0430

    description addded

--- app/Helpers/Cart/CartServiceProvider.php
+++ app/Helpers/Cart/CartServiceProvider.php
@@ -10,6 +10,7 @@ class CartServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // it means fire CardService whenever 'cart' were called
         $this->app->singleton('cart',function (){
             return new CartService();
         });
